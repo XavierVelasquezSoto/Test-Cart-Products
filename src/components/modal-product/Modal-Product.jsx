160dbe32a162865318f13821c89e142b1b5298a5
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-20 02:57:40 +0100

    Update Modal-Product.jsx

--- src/components/modal-product/Modal-Product.jsx
+++ src/components/modal-product/Modal-Product.jsx
@@ -23,40 +23,49 @@ const Modal = ({ setOpenModal, cart }) => {
 			return acc + cart.price * cart.quantity;
 		}, 0)
 		.toFixed(2);
-	return cart.map(confirmedProduct => {
-		// console.log(confirmedProduct);
-		return (
-			<StyledModalContainer key={confirmedProduct.id}>
-				<StyledModal>
-					<img src={'/assets/images/icon-order-confirmed.svg'} alt='' />
-					<StyledModalTitle>Order Confirmed</StyledModalTitle>
-					<StyledModalTextConfirmed>
-						We hope you enjoy your food!
-					</StyledModalTextConfirmed>
-					<StyledModalFlex>
-						<StyledModalImg src={confirmedProduct.images.thumbnail} alt='' />
-						<StyledPadding>
-							<StyledModalP>{confirmedProduct.name}</StyledModalP>
-							<StyledModalSpan1>€{confirmedProduct.quantity}</StyledModalSpan1>
-							<StyledModalSpan2>
-								@ €{confirmedProduct.price.toFixed(2)}
-							</StyledModalSpan2>
-						</StyledPadding>
-						<StyledModalSpan3>
-							€{confirmedProduct.price * confirmedProduct.quantity}
-						</StyledModalSpan3>
-					</StyledModalFlex>
-					<StyledModalOrderTotalDiv>
-						<StyledModalOrderTotal1>Order total</StyledModalOrderTotal1>
-						<StyledModalOrderTotal2>€{sortModal}</StyledModalOrderTotal2>
-					</StyledModalOrderTotalDiv>
-					<StyledModalButton onClick={() => setOpenModal(false)}>
-						Start New Order
-					</StyledModalButton>
-				</StyledModal>
-			</StyledModalContainer>
-		);
-	});
+	// console.log(confirmedProduct);
+	return (
+		<StyledModalContainer>
+			<StyledModal>
+				<img src={'/assets/images/icon-order-confirmed.svg'} alt='' />
+				<StyledModalTitle>Order Confirmed</StyledModalTitle>
+				<StyledModalTextConfirmed>
+					We hope you enjoy your food!
+				</StyledModalTextConfirmed>
+				<StyledModalFlex>
+					{cart.map(confirmedProduct => {
+						return (
+							<div key={confirmedProduct.id}>
+								<StyledModalImg
+									src={confirmedProduct.images.thumbnail}
+									alt=''
+								/>
+								<StyledPadding>
+									<StyledModalP>{confirmedProduct.name}</StyledModalP>
+									<StyledModalSpan1>
+										€{confirmedProduct.quantity}
+									</StyledModalSpan1>
+									<StyledModalSpan2>
+										@ €{confirmedProduct.price.toFixed(2)}
+									</StyledModalSpan2>
+								</StyledPadding>
+								<StyledModalSpan3>
+									€{confirmedProduct.price * confirmedProduct.quantity}
+								</StyledModalSpan3>
+							</div>
+						);
+					})}
+				</StyledModalFlex>
+				<StyledModalOrderTotalDiv>
+					<StyledModalOrderTotal1>Order total</StyledModalOrderTotal1>
+					<StyledModalOrderTotal2>€{sortModal}</StyledModalOrderTotal2>
+				</StyledModalOrderTotalDiv>
+				<StyledModalButton onClick={() => setOpenModal(false)}>
+					Start New Order
+				</StyledModalButton>
+			</StyledModal>
+		</StyledModalContainer>
+	);
 };
 
 export default Modal;
